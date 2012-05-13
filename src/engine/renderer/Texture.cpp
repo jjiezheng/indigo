6744c8857fd72ce940add7aad0b53863b2f0e559
Nicholas Kostelnik <nkostelnik@gmail.com>
2012-05-13 15:12:05 +0100

    start of mips

--- src/engine/renderer/Texture.cpp
+++ src/engine/renderer/Texture.cpp
@@ -15,8 +15,26 @@ void Texture::init(const char* filePath) {
 
   Image image;
   image.load(filePath);
+  
+  
+//  int width = image.width();
+//  int height = image.height();
+//  int mipLevel = 0;
+//  unsigned int offset = 0;
+//  while (width > 1) {
+//    char* pixels = (char*)image.data();
+//    glTexImage2D(GL_TEXTURE_2D, mipLevel++, GL_RGBA, width, height, 0, image.format(), GL_UNSIGNED_BYTE, image.data());
+//    offset += width * height * 4;
+//    width /= 2;
+//    height /= 2;
+//  }
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 1);
 
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width(), image.height(), 0, image.format(), GL_UNSIGNED_BYTE, image.data());
-  glGenerateMipmap(GL_TEXTURE_2D);
+  
+  unsigned int offset = image.width() * image.height() * 4;
+  glTexImage2D(GL_TEXTURE_2D, 1, GL_RGBA, image.width() / 2.0f, image.height() / 2.0f, 0, image.format(), GL_UNSIGNED_BYTE, (char*)image.data() + offset);
+//  glGenerateMipmap(GL_TEXTURE_2D);
 }
  
