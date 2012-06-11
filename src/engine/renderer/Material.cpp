bf25f555d366ac34e20195910e88ddc504e87f04
Nick Kostelnik <nkostelnik@gmail.com>
2012-06-11 13:57:59 +0100

    light working, normals gone funny

--- src/engine/renderer/Material.cpp
+++ src/engine/renderer/Material.cpp
@@ -5,21 +5,23 @@
 #include "SceneContext.h"
 
 #include "GraphicsInterface.h"
+#include "maths/Matrix3x3.h"
 
 void Material::bind(IViewer* camera, const Matrix4x4& model, const Matrix3x3& normalMatrix, const SceneContext& sceneContext, IEffect* effect) const {  
   effect->setUniform(model, "World");
   
   Matrix4x4 worldView = camera->viewTransform() * model;
   effect->setUniform(worldView, "WorldView");
+
+  Matrix4x4 normMatrix = camera->viewTransform().inverse();
+  effect->setUniform(normMatrix.mat3x3(), "NormalMatrix");
   
   Matrix4x4 worldViewProjection = camera->projection() * worldView;
   effect->setUniform(worldViewProjection, "WorldViewProj");
 
   Matrix4x4 viewProjection = camera->projection() * camera->viewTransform();
   effect->setUniform(viewProjection, "ViewProj");
-  effect->setUniform(viewProjection.inverse(), "ViewProjInv");
-  
-  effect->setUniform(normalMatrix, "NormalMatrix");
+  effect->setUniform(viewProjection.inverse().transpose(), "ViewProjInv");
  
   std::map<std::string, Texture>::const_iterator tit = textures_.begin(); 
   for (; tit != textures_.end(); ++tit) {
