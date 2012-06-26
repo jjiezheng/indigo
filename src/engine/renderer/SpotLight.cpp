2ce7feb89d71815b2ae084fc253479162f56858e
Nick Kostelnik <nkostelnik@gmail.com>
2012-06-26 13:48:31 +0100

    got the light rotating correctly, now something wrong with view/projection

--- src/engine/renderer/SpotLight.cpp
+++ src/engine/renderer/SpotLight.cpp
@@ -4,6 +4,8 @@
 
 #include "GraphicsInterface.h"
 
+#include "maths/Angles.h"
+
 Matrix4x4 SpotLight::transform() const {
   float xzScale = tan(outerAngle_ / 2.0f) * length_ * 2;
   return Matrix4x4::translation(position_) * rotation() * orientation_ * Matrix4x4::scale(Vector4(xzScale, length_, xzScale));
@@ -11,9 +13,8 @@ Matrix4x4 SpotLight::transform() const {
 
 Matrix4x4 SpotLight::rotation() const {
   Vector4 direction = direction_.normalize();
-
   // this is all out of whack, it needs to face forwards first, then be orientated, I think the rotation rotating in the wrong direction too
-  Vector4 origin = Vector4(0, -1.0, 0.0, 1);
+  Vector4 origin = Vector4::FORWARD;
   Vector4 axisRaw = origin.cross(direction);
   Vector4 axis = axisRaw.normalize();
   float angle = origin.angle(direction);
