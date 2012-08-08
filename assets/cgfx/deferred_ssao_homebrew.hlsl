c285d6e32e85d1bfa63b376ec3c44b0ed4bbb8d4
Nick Kostelnik <nkostelnik@gmail.com>
2012-08-08 17:31:14 +0100

    bla

--- assets/cgfx/deferred_ssao_homebrew.hlsl
+++ assets/cgfx/deferred_ssao_homebrew.hlsl
@@ -38,6 +38,11 @@ VOutput vs(float4 position 	: POSITION,
     return OUT;
 }
 
+bool cmpa(float3 v1, float3 v2) {
+	float3 result = v1 && v2;
+	return result.x && result.y && result.z;
+}
+
 float4 ps(float4 position 		: SV_POSITION,
 		  float2 texCoord 		: TEXCOORD0) : SV_TARGET0 {
 	float4 normalData = NormalMap.Sample(NormalMapSamplerState, texCoord);
@@ -55,7 +60,12 @@ float4 ps(float4 position 		: SV_POSITION,
 	float4 positionView = positionViewRaw / positionViewRaw.w;
 
 	float3 forward = normal;
-	float3 up = float3(0, 1, 0);
+	float3 up = float3(0.0f, 1.0f, 0.0f);
+
+	if (dot(forward, up) == 1) {
+		up = float3(0, 0, -1);
+	} 
+
 	float3 right = cross(up, forward);
 	up = cross(forward, right);
 
@@ -63,31 +73,20 @@ float4 ps(float4 position 		: SV_POSITION,
 
 	float occlusionContribution = 0;
 
+	int sampleSize = 1;
+
 	float3 samples[] = {
-		{-0.60741, -0.547166, -0.559435},
-		{0.704581, 0.824213, -0.975768},
-		{-0.237953, 0.798456, -0.0737632},
-		{-0.861385, 0.811029, -0.287362},
-		{0.788018, -0.905148, -0.477126},
-		{0.175329, -0.304605, -0.421216},
-		{0.508957, 0.498031, -0.315378},
-		{0.736198, -0.502426, -0.106967},
-		{0.362774, 0.123631, -0.458754},
-		{0.211524, 0.586596, -0.418867},
-		{-0.486984, -0.471969, -0.189367},
-		{0.0836512, 0.364849, -0.416242},
-		{0.99884, 0.282937, -0.80401},
-		{0.521592, 0.851741, -0.718375},
-		{0.591357, 0.491195, -0.211127},
-		{0.571703, 0.790704, -0.323344}
+		{0, 1, -1}
 	};
 
-	float radius = 0.1f;
+	float radius = 1;
 
-	for (int i = 0; i < KernelSize; i++) {
+	for (int i = 0; i < sampleSize; i++) {
 		float3 sample = samples[i] * radius;
 
 		float3 sampleAtNormalBasis = mul(sample, normalBasis);
+
+		return float4(sampleAtNormalBasis, 1.0f);
 		float3 sampleAtViewPosition = positionView + sampleAtNormalBasis;
 
 		float4 sampleScreenRaw = mul(Projection, positionView);
@@ -100,7 +99,7 @@ float4 ps(float4 position 		: SV_POSITION,
 		float sampleDepth = DepthMap.Sample(DepthMapSamplerState, sampleTexCoord).r;
 
 		// the depths are coming out the same
-		return float4(depth, sampleDepth, depth - sampleDepth, 1.0f);
+		//return float4(depth, sampleDepth, depth - sampleDepth, 1.0f);
 
 		if (depth - sampleDepth > 0) {
 			occlusionContribution += 1;
