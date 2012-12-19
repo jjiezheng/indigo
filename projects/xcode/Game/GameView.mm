bc7b0f7c5f3b7717d818acfd9caabdde09bd0ca4
Nicholas Kostelnik <nkostelnik@gmail.com>
2012-12-19 06:46:55 +0000

    forgot to check in the project

---
+++ projects/xcode/Game/GameView.mm
@@ -0,0 +1,56 @@
+#import "GameView.h"
+
+@implementation GameView
+
+- (void)dealloc {
+  // Release the display link
+  CVDisplayLinkRelease(displayLink);
+  [super dealloc];
+}
+
+- (void)prepareOpenGL {
+  [self.window setDelegate:self];
+
+  NSArray *arguments = [[NSProcessInfo processInfo] arguments];
+  const char* sceneFile = [[arguments objectAtIndex:1] cStringUsingEncoding:NSUTF8StringEncoding];
+  
+  app_.init(sceneFile);
+  
+  // Synchronize buffer swaps with vertical refresh rate
+  GLint swapInt = 1;
+  
+  [[self openGLContext] setValues:&swapInt forParameter:NSOpenGLCPSwapInterval];
+  
+  // Create a display link capable of being used with all active displays
+  CVDisplayLinkCreateWithActiveCGDisplays(&displayLink);
+  
+  // Set the renderer output callback function
+  CVDisplayLinkSetOutputCallback(displayLink, &MyDisplayLinkCallback, self);
+  
+  // Set the display link for the current renderer
+  CGLContextObj cglContext = (CGLContextObj)[[self openGLContext] CGLContextObj];
+  CGLPixelFormatObj cglPixelFormat = (CGLPixelFormatObj)[[self pixelFormat] CGLPixelFormatObj];
+  CVDisplayLinkSetCurrentCGDisplayFromOpenGLContext(displayLink, cglContext, cglPixelFormat);
+  
+  // Activate the display link
+  CVDisplayLinkStart(displayLink);
+}
+
+// This is the renderer output callback function
+static CVReturn MyDisplayLinkCallback(CVDisplayLinkRef displayLink, const CVTimeStamp* now, const CVTimeStamp* outputTime, CVOptionFlags flagsIn, CVOptionFlags* flagsOut, void* displayLinkContext) {
+  CVReturn result = [(GameView*)displayLinkContext getFrameForTime:outputTime];
+  return result;
+}
+
+- (CVReturn)getFrameForTime:(const CVTimeStamp*)outputTime {
+  // Add your drawing codes here
+  app_.mainLoop();
+  return kCVReturnSuccess;
+}
+
+- (void)windowWillClose:(NSNotification *)notification {
+  app_.destroy();
+}
+
+
+@end
