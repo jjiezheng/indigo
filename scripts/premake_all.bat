b8806ea1e1634dd379eff4ed5fbfd0dbeb363972
Nick Kostelnik <nkostelnik@gmail.com>
2013-01-02 11:46:46 +0000

    resident windows premake

--- scripts/premake_all.bat
+++ scripts/premake_all.bat
@@ -1,6 +1,6 @@
-premake4 --platform=x32 vs2008
-premake4 --platform=x32 vs2010
-premake4 --platform=x32 vs2012
-premake4 --platform=ps3 vs2008
-premake4 --platform=ps3 vs2010
-premake4 --platform=ps3 vs2012
+%~dp0/premake4.exe --platform=x32 vs2008
+%~dp0/premake4.exe --platform=x32 vs2010
+%~dp0/premake4.exe --platform=x32 vs2012
+%~dp0/premake4.exe --platform=ps3 vs2008
+%~dp0/premake4.exe --platform=ps3 vs2010
+%~dp0/premake4.exe --platform=ps3 vs2012
