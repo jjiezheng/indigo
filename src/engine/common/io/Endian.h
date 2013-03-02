5cf17ba7e8dbba1cc307cc8d00c67149dcb37083
Nicholas Kostelnik <nkostelnik@gmail.com>
2013-03-02 12:35:49 +0000

    windows working

--- src/engine/common/io/Endian.h
+++ src/engine/common/io/Endian.h
@@ -2,7 +2,18 @@
 #define ENDIAN_H
 
 
+#ifdef PLATFORM_WINDOWS
+typedef __int16 int16_t;
+typedef unsigned __int16 uint16_t;
+
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
+#else
 #include <stdint.h>
+#endif
 
 //! Byte swap unsigned short
 uint16_t swap_uint16(uint16_t val);
